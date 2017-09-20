fc8c4a3e46f9fb1809a2ae0129acdf33c6ee61cb
Nick Brown <nrbrown87@gmail.com>
2017-09-20 10:58:23 -0700

    some notes from tidy_tools

--- test1.R
+++ test1.R
@@ -1,1 +1,9 @@
-hi
+str(got_chars)
+x<-map(got_chars, "titles")
+
+
+x<-tibble::tibble(
+  name = map_chr(got_chars,"name"),
+  id = map_int(got_chars,"id"),
+  alive = map_lgl(got_chars,"alive"),
+  gender = map_chr(got_chars,"gender"))
